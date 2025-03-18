8b8f8d33423911701f6fc4fb0ef72c1528945f9c
Daniel Alvaro <153893558+danielta18@users.noreply.github.com>
2025-03-18 16:56:35 +0000

    fixing reminders9

--- app/scheduler.py
+++ app/scheduler.py
@@ -3,5 +3,5 @@ from app.tasks import send_reminders
 
 def start_reminder_scheduler(app):
     scheduler = BackgroundScheduler()
-    scheduler.add_job(send_reminders, 'interval', minutes=1, args=[app])
+    scheduler.add_job(send_reminders, 'interval', minutes=1, args=[app], coalesce=True, max_instances=1)
     scheduler.start()
