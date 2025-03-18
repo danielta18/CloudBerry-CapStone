6b731a04eb0888a4b28cc84bf382bc79464b3b61
Daniel Alvaro <153893558+danielta18@users.noreply.github.com>
2025-03-18 00:37:14 +0000

    fixing login redirect3

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.config import Config
 from app.extensions import db, migrate, login_manager, mail
 from app.routes import main_bp
+from app.models import User  # Import User model here for user_loader
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +13,11 @@ def create_app():
     migrate.init_app(app, db)
     login_manager.init_app(app)
     mail.init_app(app)
+
+    # Add user_loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))  # Use the User model here
 
     # Register blueprints
     app.register_blueprint(main_bp)
--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_mail import Mail
-from app.models import User
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,7 +9,3 @@ login_manager = LoginManager()
 mail = Mail()
 
 login_manager.login_view = 'main.login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
