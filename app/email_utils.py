529680c6c52a6bd2e993486c9dcb5d4dc0a09bed
Daniel Alvaro <153893558+danielta18@users.noreply.github.com>
2025-03-18 05:43:14 +0000

    fixing reminders

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,9 +1,10 @@
 from flask_mail import Message
 from app.extensions import mail
+from app.config import Config
 
 def send_email(to, subject, body):
     try:
-        msg = Message(subject, sender="yourapp@example.com", recipients=[to])
+        msg = Message(subject, sender=Config.MAIL_DEFAULT_SENDER, recipients=[to])
         msg.body = body
         mail.send(msg)
         print(f"Reminder sent to {to}")
